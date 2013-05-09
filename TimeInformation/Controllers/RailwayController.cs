c247ebf591d2b084533e746282f992c634063050
Calvert Yang <yz3210@gmail.com>
2013-05-09 17:25:35 +0800

    Add api controller and modify view

---
+++ TimeInformation/Controllers/RailwayController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TimeInformation.Models;
+
+namespace TimeInformation.Controllers
+{
+    public class RailwayController : ApiController
+    {
+        // GET api/railway
+        public IEnumerable<string> Get(string FromStation, string ToStation, DateTime GetinDate)
+        {
+            string path = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/StationList.json");
+
+            // Deserialize json file to list
+            List<StationList> stationList = JsonConvert.DeserializeObject<List<StationList>>(
+                File.ReadAllText(path)).Select(c => (StationList)c).ToList();
+            
+            string fromStationCode = stationList.Where(s => s.zh_TW == FromStation || s.en_US == FromStation).FirstOrDefault().Station;
+            string toStationCode = stationList.Where(s => s.zh_TW == ToStation || s.en_US == ToStation).FirstOrDefault().Station;
+
+
+            return new string[] { fromStationCode, toStationCode, GetinDate.ToShortDateString() };
+        }
+
+        // GET api/railway/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/railway
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/railway/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/railway/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
